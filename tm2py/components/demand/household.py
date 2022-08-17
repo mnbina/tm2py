8f8eb28d474c7eead9263e3091db0f113e9fedc8
Jayne Chang <orzmath@gmail.com>
2022-08-17 23:34:03 +0000

    Revert "adding sample rate setup"
    This reverts commit 69cb8eacf99f84d070b483de54753fa18fe006da.

--- tm2py/components/demand/household.py
+++ tm2py/components/demand/household.py
@@ -19,17 +19,16 @@ class HouseholdModel(Component):
         """Run the the household resident travel demand model.
 
         Steps:
-            0. Updates (or calculates) telecommute constants
             1. Starts household manager.
             2. Starts matrix manager.
             3. Starts resident travel model (CTRAMP).
             4. Cleans up CTRAMP java.
         """
-        self._update_telecommute_constants()
         self._start_household_manager()
         self._start_matrix_manager()
         self._start_jppf_driver()
         self._start_jppf_node0()
+        self._update_telecommute_constants()
         self._run_resident_model()
         self._stop_java()
 
@@ -38,7 +37,7 @@ class HouseholdModel(Component):
             "cd /d {}".format(self.controller.config.run.ctramp_run_dir),
             "CALL {}\\CTRAMP\\runtime\\SetPath.bat".format(self.controller.config.run.ctramp_run_dir),
             "set HOST_IP={}".format(self.controller.config.run.host_ip_address),
-            "start \"Household Manager\" java -Xms200m -Xmx20000m -Dlog4j.configuration=log4j_hh.xml com.pb.mtc.ctramp.MtcHouseholdDataManager -hostname %HOST_IP%",
+            "start \"Household Manager\" java -Xms20000m -Xmx20000m -Dlog4j.configuration=log4j_hh.xml com.pb.mtc.ctramp.MtcHouseholdDataManager -hostname %HOST_IP%",
             "echo Hello World"
         ]
         run_process(commands, name="start_household_manager")
@@ -48,13 +47,13 @@ class HouseholdModel(Component):
             "cd /d {}".format(self.controller.config.run.ctramp_run_dir),
             "CALL {}\\CTRAMP\\runtime\\SetPath.bat".format(self.controller.config.run.ctramp_run_dir),
             "set HOST_IP={}".format(self.controller.config.run.host_ip_address),
-            "start \"Matrix Manager\" java -Xms32000m -Xmx128000m -Dlog4j.configuration=log4j_mtx.xml -Djava.library.path=\"CTRAMP/runtime\" com.pb.models.ctramp.MatrixDataServer -hostname %HOST_IP%",
+            "start \"Matrix Manager\" java -Xms14000m -Xmx140000m -Dlog4j.configuration=log4j_mtx.xml -Djava.library.path=\"CTRAMP/runtime\" com.pb.models.ctramp.MatrixDataServer -hostname %HOST_IP%",
         ]
         run_process(commands, name="start_matrix_manager")
 
     def _run_resident_model(self):
-        sample_rate_iteration_values = self.controller.config.run.sample_rate_iteration
-        sample_rate_iteration = dict(zip(range(1, len(sample_rate_iteration_values) +1), sample_rate_iteration_values))
+        #sample_rate_iteration = {1: 0.3, 2: 0.5, 3: 1, 4: 0.02, 5: 0.02}
+        sample_rate_iteration = {1: 0.01, 2: 0.01}
         iteration = self.controller.iteration
         sample_rate = sample_rate_iteration[iteration]
         seed = 0
@@ -62,7 +61,7 @@ class HouseholdModel(Component):
         commands = [
             "cd /d {}".format(self.controller.config.run.ctramp_run_dir),
             "CALL {}\\CTRAMP\\runtime\\SetPath.bat".format(self.controller.config.run.ctramp_run_dir),
-            "java -showversion -Xmx64000m -cp %CLASSPATH% -Dlog4j.configuration=log4j.xml -Djava.library.path=%RUNTIME% -Djppf.config=jppf-clientDistributed.properties "
+            "java -showversion -Xmx6000m -cp %CLASSPATH% -Dlog4j.configuration=log4j.xml -Djava.library.path=%RUNTIME% -Djppf.config=jppf-clientDistributed.properties "
             "com.pb.mtc.ctramp.MtcTourBasedModel mtcTourBased -iteration {} -sampleRate {} -sampleSeed {}".format(iteration, sample_rate, seed),
         ]
         run_process(commands, name="run_resident_model")
@@ -72,7 +71,7 @@ class HouseholdModel(Component):
             "cd /d {}".format(self.controller.config.run.ctramp_run_dir),
             "CALL {}\\CTRAMP\\runtime\\SetPath.bat".format(self.controller.config.run.ctramp_run_dir),
             "set HOST_IP={}".format(self.controller.config.run.host_ip_address),
-            "start \"JPPF Server\" java -server -Xmx16000m -Dlog4j.configuration=log4j-driver.properties -Djppf.config=jppf-driver.properties org.jppf.server.DriverLauncher",
+            "start \"JPPF Server\" java -server -Xmx16m -Dlog4j.configuration=log4j-driver.properties -Djppf.config=jppf-driver.properties org.jppf.server.DriverLauncher",
         ]
         run_process(commands, name="start_jppf_driver")
     
@@ -81,7 +80,7 @@ class HouseholdModel(Component):
             "cd /d {}".format(self.controller.config.run.ctramp_run_dir),
             "CALL {}\\CTRAMP\\runtime\\SetPath.bat".format(self.controller.config.run.ctramp_run_dir),
             "set HOST_IP={}".format(self.controller.config.run.host_ip_address),
-            "start \"Node 0\" java -server -Xmx64000m -Dlog4j.configuration=log4j-node0.xml -Djppf.config=jppf-node0.properties org.jppf.node.NodeLauncher",
+            "start \"Node 0\" java -server -Xmx128m -Dlog4j.configuration=log4j-node0.xml -Djppf.config=jppf-node0.properties org.jppf.node.NodeLauncher",
         ]
         run_process(commands, name="start_jppf_node")
 
@@ -93,10 +92,10 @@ class HouseholdModel(Component):
         commands = [
         "cd /d {}".format(self.controller.config.run.ctramp_run_dir),
         "CALL {}\\CTRAMP\\runtime\\SetPath.bat".format(self.controller.config.run.ctramp_run_dir),
-        ] + [
+        "cd %PYTHON_PATH%",] + [
         f"set {key}={telecommute_parameters[key]}" for key in telecommute_parameters
         ] + [
-        "%PYTHON_PATH%\\python {}\\CTRAMP\\scripts\\preprocess\\updateTelecommuteConstants.py".format(self.controller.config.run.ctramp_run_dir),
-        "copy /Y main\\telecommute_constants_00.csv main\\telecommute_constants.csv"
+        "python CTRAMP\\scripts\\preprocess\\updateTelecommuteConstants.py",
+        "copy /Y main\telecommute_constants_00.csv main\telecommute_constants.csv"
         ]
         run_process(commands, name="update_telecommute_constants")
